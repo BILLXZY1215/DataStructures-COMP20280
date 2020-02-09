953de87558f28eeeee22a45a91e62f5f8f9076dd
AhmedJouda2000 <ahmed_judah@yahoo.com>
2020-02-09 13:28:39 +0000

    SLL finished

--- src/projectCode20280/SinglyLinkedList.java
+++ src/projectCode20280/SinglyLinkedList.java
@@ -3,46 +3,53 @@ package projectCode20280;
 import java.util.Iterator;
 
 public class SinglyLinkedList<E> implements List<E> {
-	
+
 	private Node<E> head = null;
 	private int size = 0;
-	
-	//empty constructor
-	public SinglyLinkedList() {}
-	
+
+	// empty constructor
+	public SinglyLinkedList() {
+	}
+
 	private static class Node<E> {
 		private E element;
 		private Node<E> next;
-		
+
 		public Node(E e, Node<E> n) {
 			element = e;
 			next = n;
 		}
-		
-		//Accessor Methods
-		public E getElement() {return element;}
-		public Node<E> getNext() {return next;}
-		//Modifier Methods
-		public void setNext(Node<E> n) { next = n;}
+
+		// Accessor Methods
+		public E getElement() {
+			return element;
+		}
+
+		public Node<E> getNext() {
+			return next;
+		}
+
+		// Modifier Methods
+		public void setNext(Node<E> n) {
+			next = n;
+		}
 	}
-	
-	//checks if the LL is empty
+
+	// checks if the LL is empty
 	@Override
 	public boolean isEmpty() {
 		return size == 0;
 	}
-						
-	
+
 	@Override
 	public E get(int i) {
-		
+
 		Node<E> curr = head;
-		for(int j = 0; j<i; j++) {
-			
+		for (int j = 0; j < i; j++) {
+
 			curr = curr.getNext();
 		}
-		
-		
+
 		return curr.getElement();
 	}
 
@@ -51,15 +58,15 @@ public class SinglyLinkedList<E> implements List<E> {
 		Node<E> newNode = new Node<E>(e, null);
 		Node<E> prevNode = null;
 		Node<E> currNode = head;
-		
-		for(int j=0; j<i; j++) {
+
+		for (int j = 0; j < i; j++) {
 			prevNode = currNode;
 			currNode = currNode.next;
 		}
-		
+
 		prevNode.setNext(newNode);
 		newNode.setNext(currNode);
-		
+
 		size++;
 	}
 
@@ -69,29 +76,29 @@ public class SinglyLinkedList<E> implements List<E> {
 			throw new RuntimeException("cant delete");
 		}
 		
-		if (head.getElement().equals(i)) {
-			head= head.next;
-			size--;
-			return null;
+		Node<E> removedNode = head;
+		if (i== 0) {
+			removeFirst();
+			return removedNode.getElement();
 		}
-		
+
 		Node<E> cur = head;
 		Node<E> prev = null;
-		
-		for(int j = 0; j<i; j++) {
+
+		for (int j = 0; j < i; j++) {
 			prev = cur;
 			cur = cur.getNext();
 		}
-		
+		removedNode = cur;
 		prev.next = cur.next;
 		size--;
-		return null;
+		return removedNode.getElement();
 	}
 
 	@Override
 	public Iterator<E> iterator() {
 		Node<E> cu = head;
-		
+
 		while (cu.next != null) {
 			System.out.print(cu.getElement() + ", ");
 			cu = cu.next;
@@ -102,44 +109,46 @@ public class SinglyLinkedList<E> implements List<E> {
 	@Override
 	public int size() {
 		return size;
-	}	
-	
+	}
 
 	@Override
 	public E removeFirst() {
 		if (isEmpty()) {
-            return null;
-        }
-        if (size != 0) {
-            head = head.next;
-            size--;
-        }
+			return null;
+		}
+		Node<E> removedNode = head;
+		if (size != 0) {
+			
+			head = head.next;
+			size--;
+		}
 
-        return null;
+		return removedNode.getElement();
 	}
 
 	@Override
 	public E removeLast() {
 		if (head == null || head.next == null) {
-            return null;
-        }
-		
-		Node<E> second_last = head;
-		
-		while (second_last.next.next != null) {
-			second_last = second_last.next;			
+			return null;
 		}
+
+		Node<E> second_last = head;
+
+		while (second_last.next.next != null) {
+			second_last = second_last.next;
+		}
+		Node<E> removedNode = second_last.getNext();
 		second_last.next = null;
 		size--;
-        
-        return null;
+		
+		return removedNode.getElement();
 	}
 
 	@Override
 	public void addFirst(E e) {
-		head= new Node<E>(e, head);
+		head = new Node<E>(e, head);
 		size++;
-		
+
 	}
 
 	@Override
@@ -147,38 +156,32 @@ public class SinglyLinkedList<E> implements List<E> {
 		Node<E> newNode = new Node<E>(e, null);
 		Node<E> last = head;
 
-        while (last.getNext() != null) {
+		while (last.getNext() != null) {
 			last = last.getNext();
 		}
-        last.setNext(newNode);
-        size++;
-		
+		last.setNext(newNode);
+		size++;
+
 	}
-	
-	
-    
-	
+
 	@Override
 	public String toString() {
 		String newString = "[ ";
-		
+
 		Node<E> current = head;
 		while (current != null) {
-			newString = newString+ current.getElement();
-			newString = newString+ "->";
-			
+			newString = newString + current.getElement();
+			newString = newString + "->";
+
 			if (current.getNext() == null) {
-				newString = newString+ "NULL]";
+				newString = newString + "NULL]";
 			}
-			
+
 			current = current.getNext();
 		}
-		
-		
+
 		return newString;
 	}
-
-	
 
 	public static void main(String[] args) {
 		String[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
@@ -192,43 +195,41 @@ public class SinglyLinkedList<E> implements List<E> {
 
 		sll.removeFirst();
 		System.out.println(sll.toString());
-		
+
 		sll.removeLast();
 		System.out.println(sll.toString());
 
-		sll.remove(2);
+		System.out.println("------"+sll.remove(49));
 		System.out.println(sll.toString());
-		
-		/*for (String s : sll) {
-			System.out.print(s + ", ");
-		}*/
+
+		/*
+		 * for (String s : sll) { System.out.print(s + ", "); }
+		 */
 		System.out.println(sll.get(2));
-		
+
 		sll.add(2, "j");
 		System.out.println(sll.toString());
-		
+
 		sll.addFirst("j");
 		System.out.println(sll.toString());
-		
+
 		sll.addLast("j");
 		System.out.println(sll.toString());
 		System.out.println("*************************************");
-		
-		 SinglyLinkedList<Integer> ll = new SinglyLinkedList<Integer>();
-         ll.addFirst(0);
-         ll.addFirst(1);
-         ll.addLast(9);
-         ll.addFirst(2);
-         ll.addLast(-1);
-         System.out.println(ll);
-         System.out.println("******" + ll.get(2));
-         ll.add(2, 3);
-         System.out.println(ll);
-         System.out.println(ll.size());
-         ll.removeFirst();
-         System.out.println(ll);
-         System.out.println(ll.size());
-	}
-	}
 
-
+		SinglyLinkedList<Integer> ll = new SinglyLinkedList<Integer>();
+		ll.addFirst(0);
+		ll.addFirst(1);
+		ll.addLast(9);
+		ll.addFirst(2);
+		ll.addLast(-1);
+		System.out.println(ll);
+		System.out.println("******" + ll.get(2));
+		ll.add(2, 3);
+		System.out.println(ll);
+		System.out.println(ll.size());
+		ll.removeFirst();
+		System.out.println(ll);
+		System.out.println(ll.size());
+	}
+}
