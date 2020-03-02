4002996dd48cad0bcc93847efa393942a32235d9
AhmedJouda2000 <ahmed_judah@yahoo.com>
2020-03-02 10:37:40 +0000

    bracktes

--- src/projectCode20280/ArrayQueue.java
+++ src/projectCode20280/ArrayQueue.java
@@ -2,39 +2,60 @@ package projectCode20280;
 
 public class ArrayQueue<E> implements Queue<E> {
 	
+	private static final int CAPACITY = 10;	//default capacity if user doesn't provide one during queue creation
+	//instance variables
 	private int size = 0;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int firstElement = 0;
+	private E[] data;
+	
+	//constructors
+	public ArrayQueue() {this(CAPACITY);}	//constructs queue with default capacity if no capacity passed to the object
+	
+	public ArrayQueue(int capacity) {
+		data = (E[]) new Object[capacity];
 	}
 
+	//methods
 	@Override
-	public int size() {
-		return size;
-	}
+	public int size() {return size;}
 
 	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean isEmpty() {return size==0;}
 
 	@Override
-	public void enqueue(E e) {
-		// TODO Auto-generated method stub
-		
+	public void enqueue(E e) throws IllegalStateException {
+		if (size == data.length) {
+			throw new IllegalStateException("Queue is full");
+		}
+		int avail = (firstElement + size)% data.length;
+		data[avail] = e;
+		size++;
 	}
 
 	@Override
 	public E first() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) {
+			return null;
+		}
+		return data[firstElement];
 	}
 
 	@Override
 	public E dequeue() {
+		if (isEmpty()) {
+			return null;
+		}
+		E answer = data[firstElement];
+		data[firstElement] = null;
+		firstElement = (firstElement + 1)%data.length;
+		size--;
+		return answer;
+	}
+	
+	
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		return null;
+
 	}
 
 }
--- src/projectCode20280/LinkedQueue.java
+++ src/projectCode20280/LinkedQueue.java
@@ -1,40 +1,33 @@
 package projectCode20280;
 
 public class LinkedQueue<E> implements Queue<E> {
+	
+	private SinglyLinkedList<E> list = new SinglyLinkedList<>();	//empty list
+	
+	//constructor...relies on empty list
+	public LinkedQueue() {}
+	
 
+	@Override
+	public int size() {return list.size();}
+
+	@Override
+	public boolean isEmpty() {return list.isEmpty();}
+
+	@Override
+	public void enqueue(E e) {list.addLast(e);}
+
+	@Override
+	public E first() {return list.get(0);}
+
+	@Override
+	public E dequeue() {return list.removeFirst();}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void enqueue(E e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public E first() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public E dequeue() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
